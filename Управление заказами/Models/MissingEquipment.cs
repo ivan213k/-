1487b3302c483045f0482b47ad6f8ab3e2d46ab8
Іван Захарук <zaharuck@outlook.com>
2018-12-12 18:30:23 +0200

    Реализована возможность просмотра информации об оборудовании которого не хватает в процессе создания заказа

---
+++ Управление заказами/Models/MissingEquipment.cs
@@ -0,0 +1,18 @@
+﻿namespace Управление_заказами.Models
+{
+    class MissingEquipment
+    {
+        public string Name { get; set; }
+
+        public int NeedCount { get; set; }
+
+        public int Balance { get; set; }
+
+        public int AvalibleInSelectedDateRange { get; set; }
+
+        public int NotEnough
+        {
+            get { return NeedCount - Balance + AvalibleInSelectedDateRange; }
+        }
+    }
+}
---
+++ Управление заказами/ViewModels/Converters/DateTimeToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Управление_заказами.ViewModels.Converters
+{
+    class DateTimeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((DateTime)value).ToString(new CultureInfo("Uk-Ua"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
