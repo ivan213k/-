e2b8747bbb5d63e314267ff94efe2e5d1b031ef9
Іван Захарук <zaharuck@outlook.com>
2019-01-04 20:48:23 +0200

    Реализовано изменение имен оборудования в аренде при редактировании оборудования на складе

--- Управление заказами/Models/Core/EquipmentInfo.cs
+++ Управление заказами/Models/Core/EquipmentInfo.cs
@@ -9,6 +9,41 @@ namespace Управление_заказами.Models.Core
 {
     class EquipmentInfo : IEquipmentInfo
     {
+        private void UpdateEquipmensNameInRent(List<EquipmentInStock> newEquipments)
+        {
+            using (AppDbContext db = new AppDbContext())
+            {
+                var oldEquipments = db.EquipmentsInStock.ToList();
+                foreach (var newEquipment in newEquipments)
+                {
+                    string oldEquipmentName = oldEquipments.Select(eq => eq).
+                        Where(eq => eq.Id == newEquipment.Id).Single().Name;
+                    UpdateEquipmentName(oldEquipmentName, newEquipment.Name, db);
+                }
+
+                db.SaveChanges();
+            }
+        }
+
+        private void UpdateEquipmentName(string oldName, string newName, AppDbContext db)
+        {
+            foreach (var equipmentInRent in db.EquipmentsInRent)
+            {
+                if (equipmentInRent.Name == oldName)
+                {
+                    equipmentInRent.Name = newName;
+                }
+            }
+
+            foreach (var equipmentFromOrder in db.EquipmentsFromOrder)
+            {
+                if (equipmentFromOrder.Name == oldName)
+                {
+                    equipmentFromOrder.Name = newName;
+                }
+            }
+        }
+
         public async Task<List<EquipmentInStock>> GetEquipmentsAsync()
         {
             return await Task.Factory.StartNew(() =>
@@ -53,6 +88,7 @@ namespace Управление_заказами.Models.Core
             {
                 using (AppDbContext db = new AppDbContext())
                 {
+                    UpdateEquipmensNameInRent(equipments);
                     db.EquipmentsInStock.UpdateRange(equipments);
                     db.SaveChanges();
                 }
