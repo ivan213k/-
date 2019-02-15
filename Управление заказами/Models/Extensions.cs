125a4ca6711e1e0ab48a3b73909ba1d48d525d67
Іван Захарук <zaharuck@outlook.com>
2019-02-15 16:20:02 +0200

    Удалено обновление всего списка при удалении оборудования в окне "Оборудование на складе"

---
+++ Управление заказами/Models/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Управление_заказами.Models
+{
+    static class Extensions
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this List<T> list)
+        {
+            return new ObservableCollection<T>(list);
+        }
+    }
+}
